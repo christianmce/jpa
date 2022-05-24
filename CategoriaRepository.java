310bb83d27ce6d55aec7961c3ed16509e656b7fb
ChristianMCE <cmce@unach.mx>
2022-05-23 20:13:26 -0500

    Create CategoriaRepository.java

---
+++ CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.app.taller.service;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import com.app.taller.entity.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>  {
+	@Query ("select o from Categoria o where o.nombreCat like %?1")
+	Categoria findByNombre(String nombre);
+	
+	@Query ("select x from Categoria x where x.nivel = ?1") 
+	List<Categoria> buscarXNivel(int niv);
+}
