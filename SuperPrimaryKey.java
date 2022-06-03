ef3757211d8563d187dbc05bb9f61c0aa080511b
ChristianMCE <cmce@unach.mx>
2022-06-03 14:49:44 -0500

    Update SuperPrimaryKey.java

--- SuperPrimaryKey.java
+++ SuperPrimaryKey.java
@@ -19,3 +19,10 @@ public class SuperLlave implements Serializable {
 	}
 
 }
+
+
+//************************* Información complementaria como resultado de ejecutar el código -----------------
+// Hibernate: create table tb_ofertas (fecha date not null, id_art integer not null, por_desc integer, primary key (fecha, id_art)) engine=MyISAM
+// Hibernate: create table tb_productos (id_art integer not null auto_increment, descripcion varchar(255), precio float, primary key (id_art)) engine=MyISAM
+// Hibernate: alter table tb_empleado add constraint FKpc587h901fmu4hil9fo73hpv0 foreign key (id_depto) references tb_departamento (id_depto)
+
